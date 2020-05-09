7d02073718b0dbcaee19607354512a2123c3c0d3
Francois Best <francois@francoisbest.com>
2020-05-09 10:33:40 +0200

    feat: Add Sentry, MDX & react-icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,8 @@
 import { createChakraNextApp } from '@47ng/chakra-next'
+import { SentryErrorBoundary } from 'src/services/sentry'
 
-export default createChakraNextApp()
+export default createChakraNextApp({
+  Providers: ({ children }) => (
+    <SentryErrorBoundary>{children}</SentryErrorBoundary>
+  )
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import { NextPage } from 'next'
 import { Container, StackCard } from '@47ng/chakra-next'
-import Text from '@chakra-ui/core/dist/Text'
+import Button from '@chakra-ui/core/dist/Button'
 
 // --
 
+const Crasher = () => {
+  throw new Error('Crasher')
+}
+
 const IndexPage: NextPage = () => {
+  const [crasher, setCrasher] = React.useState(false)
+  const run = async () => {
+    await new Promise(r => setTimeout(r, 1000))
+    throw new Error('in async method')
+  }
+
   return (
     <Container mt={12}>
       <StackCard>
-        <Text>Foo</Text>
+        <Button
+          onClick={() => {
+            throw new Error('in event handler')
+          }}
+        >
+          Throw in event handler
+        </Button>
+        <Button onClick={run}>Throw in async method</Button>
+        <Button onClick={() => setCrasher(true)}>Throw in render method</Button>
+        {crasher && <Crasher />}
       </StackCard>
     </Container>
   )
