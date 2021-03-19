5a5cebdc8ecec59035b3aa4cd27298775acf731d
Francois Best <francois@francoisbest.com>
2021-03-19 17:23:42 +0100

    feat: Add basic app layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,17 @@
-import React from 'react'
+import { Center, Text } from '@chakra-ui/react'
 import { NextPage } from 'next'
-import { StackCard } from '@47ng/chakra-next'
-import { Button, Container } from '@chakra-ui/react'
+import React from 'react'
+import { AppPageLayout } from 'src/layouts/AppPageLayout'
 
 // --
 
-const Crasher = () => {
-  throw new Error('Crasher')
-}
-
 const IndexPage: NextPage = () => {
-  const [crasher, setCrasher] = React.useState(false)
-  const run = async () => {
-    await new Promise(r => setTimeout(r, 1000))
-    throw new Error('in async method')
-  }
-
   return (
-    <Container mt={12}>
-      <StackCard>
-        <Button
-          onClick={() => {
-            throw new Error('in event handler')
-          }}
-        >
-          Throw in event handler
-        </Button>
-        <Button onClick={run}>Throw in async method</Button>
-        <Button onClick={() => setCrasher(true)}>Throw in render method</Button>
-        {crasher && <Crasher />}
-      </StackCard>
-    </Container>
+    <AppPageLayout>
+      <Center h="2xl">
+        <Text>This page is empty</Text>
+      </Center>
+    </AppPageLayout>
   )
 }
 
